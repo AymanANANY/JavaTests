83697277e567eddc1b258cb725e0fef14662ddaa
ayman ANANY <aymanenany@gmail.com>
2024-12-21 16:57:13 +0100

    ok

---
+++ JavaTests/src/javatests/JavaTests.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javatests;
+
+/**
+ *
+ * @author AAnan
+ */
+public class JavaTests {
+    public static void main(String[]args) {
+        byte b = 1;
+        char c[] = {'a'};
+        short s = 5;
+        int i = 7;
+        long l = 9;
+        float f = 1.5f;
+        double d = 70.0;
+        String name = "Ayman";
+        String n = new String(c);
+        System.out.println(n);
+    }
+}
